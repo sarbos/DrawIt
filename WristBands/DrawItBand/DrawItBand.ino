4c5b68b681a7d8248cab3504d218feef85088f78
sebastian <sarbos@rpi.edu>
2013-12-17 12:11:02 -0500

    Added blinking strip function when not yet paired.
    Signed-off-by: sebastian <sarbos@rpi.edu>

--- WristBands/DrawItBand/DrawItBand.ino
+++ WristBands/DrawItBand/DrawItBand.ino
@@ -1,4 +1,4 @@
-
+#include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
 
 #define LEDPin 10
@@ -9,6 +9,7 @@
 #define DrawB 4
 #define ModeB 3
 
+int mode = 0;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, LEDPin, NEO_GRB + NEO_KHZ800);
 SoftwareSerial mySerial(9,8);
 
@@ -21,8 +22,25 @@ void setup()
   //check if bluetooth is paired(pin6 high), if so,light red, send begin signal to computer,
   //and continue, if not, blink red until paired
 }
-
+bool led = false;
 void loop()
 {
   //check the mode, one of 3
+  if (mode ==0)
+  {
+    for (int i=0; i<6; i++)
+    {
+      if (led)
+      {
+        strip.setPixelColor(i,200,0,0);
+      }
+      else
+      {
+        strip.setPixelColor(i,0,0,0);
+      }
+    }
+    strip.show();
+    led = !led;
+  }
+  delay(200);
 }
