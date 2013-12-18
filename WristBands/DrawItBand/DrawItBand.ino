1dea52ee2d549a4e37ee059ab2581d3fc81aa67a
sebastian <sarbos@rpi.edu>
2013-12-17 19:40:16 -0500

    now blinks red until paired, shines green for a moment when paired, and then turns blue to indicate main loop is running.
    Signed-off-by: sebastian <sarbos@rpi.edu>

--- WristBands/DrawItBand/DrawItBand.ino
+++ WristBands/DrawItBand/DrawItBand.ino
@@ -8,39 +8,65 @@
 #define Bstat 6
 #define DrawB 4
 #define ModeB 3
+#define IRpin 2
 
+//the mode we are set on
+//0 = Music
+//1 = LG
+//2 = Draw
 int mode = 0;
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(6, LEDPin, NEO_GRB + NEO_KHZ800);
-SoftwareSerial mySerial(9,8);
+SoftwareSerial BT(9,8);
 
 void setup()
 {
   strip.begin();
   strip.show();
-  //need to wire up pin 32 of bt to pin 6 of attiny
   
-  //check if bluetooth is paired(pin6 high), if so,light red, send begin signal to computer,
-  //and continue, if not, blink red until paired
+  //set up pins
+  pinMode(Bkey, OUTPUT);
+  pinMode(Bstat, INPUT);
+  pinMode(DrawB, INPUT);
+  pinMode(ModeB, INPUT);
+  pinMode(IRpin, OUTPUT);
+  
+  //activate internal pullups
+  digitalWrite(DrawB, HIGH);
+  digitalWrite(ModeB, HIGH);
+  
+  //start comms to on board bt module
+  BT.begin(9600);
+  delay(500);
+  while(!digitalRead(Bstat))
+ {
+     for (int i=0; i<6; i++)
+      {
+         strip.setPixelColor(i,200,0,0);
+      }
+      strip.show();
+      delay(250);
+      for (int i=0; i<6; i++)
+      {
+          strip.setPixelColor(i,0,0,0);
+
+      }
+      strip.show();
+      delay(250);
+ }
+ for (int i=0; i<6; i++)
+  {
+     strip.setPixelColor(i,0,200,0);
+  }
+  strip.show();
+  delay(1000);
+  
 }
-bool led = false;
 void loop()
 {
-  //check the mode, one of 3
-  if (mode ==0)
+ for (int i=0; i<6; i++)
   {
-    for (int i=0; i<6; i++)
-    {
-      if (led)
-      {
-        strip.setPixelColor(i,200,0,0);
-      }
-      else
-      {
-        strip.setPixelColor(i,0,0,0);
-      }
-    }
-    strip.show();
-    led = !led;
+     strip.setPixelColor(i,0,0,200);
   }
-  delay(200);
+  strip.show();
 }
